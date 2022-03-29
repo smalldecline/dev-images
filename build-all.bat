a054dfa483c73235808c6ff76af599471e8e3fd8
smalldecline <3218125624@qq.com>
2022-03-29 14:19:45 +0800

    添加了一个一键构建脚本

---
+++ build-all.bat
@@ -0,0 +1,5 @@
+
+docker build -t devenv:base ./devenv-base
+docker build -t devenv:clang ./devenv-clang
+docker build -t devenv:python ./devenv-python
+docker build -t devenv:node.js ./devenv-nodejs
